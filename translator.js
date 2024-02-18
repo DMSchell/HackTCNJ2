97819ae7ef3b13d01b76b6c4f9cb225386a6bf0e
Davd <122733045+DMSchell@users.noreply.github.com>
2024-02-18 01:40:34 -0500

    get rid of mongo again

--- translator.js
+++
@@ -1,9 +0,0 @@
-import { write } from 'index.js';
-
-document.getElementById('signup-submit').addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the form from submitting
-    var username = document.getElementById('signup-username').value;
-    var password = document.getElementById('signup-password').value;
-    write(username, password)
-    console.log("holy shit you did it");
-});
