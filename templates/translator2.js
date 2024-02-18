f106b267e14db5059d5d76369c491408ea701023
Davd <122733045+DMSchell@users.noreply.github.com>
2024-02-18 01:19:21 -0500

    shit

--- templates/translator2.js
+++
@@ -1,7 +0,0 @@
-import { newUser } from 'translator.js';
-
-document.getElementById('signup-submit').addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the form from submitting
-    newUser();
-    console.log("yeehaw");
-});
